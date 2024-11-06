d6d3b006cba2d2b88bf9154c68f447471199f5b0
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2024-11-06 10:33:17 -0300

    Add files via upload

---
+++ quadrilatero.py
@@ -0,0 +1,7 @@
+base = float(input('Informe a medida da base:'))
+altura = float(input('Informe a medida da altura:'))
+
+if base == altura:
+    print ('O quadrilátero é um Quadrado.')
+else:
+    print ('O quadrilátero é um Retângulo.')
