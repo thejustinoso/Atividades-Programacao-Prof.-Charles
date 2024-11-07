2af9d9e75be54ec59d89d6262727082e3ebbb46b
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2024-11-06 21:48:03 -0300

    Update equação de segundo grau.py

--- equação de segundo grau.py
+++ equação de segundo grau.py
@@ -1,3 +1,4 @@
+import sys
 
 a = float(input('Digite um valor para a:'))
 b = float(input('Digite um valor para b:'))
@@ -5,12 +6,15 @@ c = float(input('Digite um valor para c:'))
 
 delta = b**2 - 4*a*c
 
-if delta < 0:
-    print('A equação não possui raízes reais.')
-elif delta == 0:
-    x = -b / (2*a)
-    print(f'A equação possui uma raiz real. x = {x:.2f}.')
+if a == 0:
+    sys.exit('O valor de "a" tem de ser diferente de zero para ser uma equação de segundo grau.')
 else:
-    x1 = (-b + delta**0.5) / (2*a)
-    x2  = (-b - delta**0.5) / (2*a)
-    print(f'A equação possui duas raízes reais: x1 = {x1:.2f} e x2 = {x2:.2f}.')
+    if delta < 0:
+        sys.exit('A equação não possui raízes reais.')
+    elif delta == 0:
+        x = -b / (2*a)
+        print(f'A equação possui uma raiz real. x = {x:.2f}.')
+    else:
+        x1 = (-b + delta**0.5) / (2*a)
+        x2  = (-b - delta**0.5) / (2*a)
+        print(f'A equação possui duas raízes reais: x1 = {x1:.2f} e x2 = {x2:.2f}.')
