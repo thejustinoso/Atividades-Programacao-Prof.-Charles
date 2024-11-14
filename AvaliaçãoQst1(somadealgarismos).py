995ba1f6f34f1fb6970b00d9a6525710ec1d51c3
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2024-11-13 21:24:19 -0300

    Update AvaliaçãoQst1(somadealgarismos).py

--- AvaliaçãoQst1(somadealgarismos).py
+++ AvaliaçãoQst1(somadealgarismos).py
@@ -1,19 +1,13 @@
-numero = str(input('Digite um número de até 4 algarismos:'))
+numero = int(input('Digite um número de até 4 algarismos:'))
 
-if numero > 0 and numero < 10:
-    print (f'O número que você digitou foi o {numero}, e como ele é unitário, não pode ser somado com outros algarismos.')
-    
-elif numero > 9 and numero < 100:
-    soma = int(algarismos[0]) + int(algarismos[1])
-    print (f'A soma dos algarismos é: {soma}')
-
-elif numero > 99 and numero < 1000:
-    soma = int(algarismos[0]) + int(algarismos[1]) + int(algarismos[2])
-    print (f'A soma dos algarismos é: {soma}')
-
-elif numero > 999 and numero < 10000:
-    soma = int(algarismos[0]) + int(algarismos[1]) + int(algarismos[3]) + int(algarismos[4])
-    print (f'A soma dos algarismos é: {soma}')
+if 0 <= numero <= 9999:
+    soma = 0
+    #Abaixo está um loop para somar cada algarismo extraido da operação "numero % 10" e remove o último algarismo do número com "numero //=10"
+    while numero > 0:
+        #Abaixo o operador "+=" atribuir o valor da operação de resto da divisão por 10 da variável numero ("umero % 10") a varável "soma"
+        soma += numero % 10
+        numero //= 10
+    print (f'O resultado da soma dos algarismos é {soma}')
 
 else:
     print ('Digite um valor válido')
