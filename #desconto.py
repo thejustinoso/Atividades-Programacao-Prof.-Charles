9214a931119c9b16af42dabe6da6730480057fbd
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2024-11-19 20:10:07 -0300

    Delete #desconto.py

--- #desconto.py
+++
@@ -1,6 +0,0 @@
-preco = float(input('Qual o valor do produto?'))
-desconto = float(input('Qual o percentual de desconto?'))
-
-valor_final = preco * desconto / 100
-
-print (f'O valor final do produto é de R${valor_final:.2f}.')
