cdb2391d618cb7b08b45a7e73a55f69bede144b6
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2024-11-21 15:41:26 -0300

    Update AvaliaçãoQst4(diascorridos).py

--- AvaliaçãoQst4(diascorridos).py
+++ AvaliaçãoQst4(diascorridos).py
@@ -4,14 +4,19 @@ diaFinal = int(input("Digite o dia final: "))
 mesFinal = int(input("Digite o mês final: "))
 
 if 1 <= diaInicial <= 31 and 1 <= diaFinal <= 31:
-    print ('Data inválida.')
-    SystemExit
+        print ('Data inválida.')
+        SystemExit
 elif 1 <= mesInicial <= 12 and 1 <= mesFinal <= 12:
-    print ('Data inválida.')
-    SystemExit 
-# Identificando se um dos meses é de 30 dias
-elif mesInicial in ['04', '06', '09', '11'] or mesFinal in ['04', '06', '09', '11']:
-    a
-# Identificando se um dos meses é Feveiro, mês com 28 dias
-elif mesInicial == '02' or mesFinal == '02':
-    a
+        print ('Data inválida.')
+        SystemExit
+else:
+    if mesInicial > mesFinal or (mesInicial == mesFinal and diaInicial > diaFinal):
+        print("Data inicial deve ser menor ou igual à data final.")
+        SystemExit
+    else:
+        # Identificando se um dos meses é de 30 dias
+        if mesInicial in ['04', '06', '09', '11'] or mesFinal in ['04', '06', '09', '11']:
+            a
+        # Identificando se um dos meses é Feveiro, mês com 28 dias
+        elif mesInicial == '02' or mesFinal == '02':
+            a
