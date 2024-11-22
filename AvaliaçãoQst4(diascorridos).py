e677cd1e509f3ab3a6a7aa89ccbecd459f4567ba
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2024-11-22 09:19:17 -0300

    Update AvaliaçãoQst4(diascorridos).py

--- AvaliaçãoQst4(diascorridos).py
+++ AvaliaçãoQst4(diascorridos).py
@@ -3,30 +3,30 @@ mesInicial = int(input("Digite o mês inicial: "))
 diaFinal = int(input("Digite o dia final: "))
 mesFinal = int(input("Digite o mês final: "))
 
-if (mesInicial = '01') or (mesFinalal = '01'):
+if mesInicial == '01' or mesFinal == '01':
 
-elif (mesInicial = '02') or (mesFinal = '02'):
+elif mesInicial == '02' or mesFinal == '02':
         
-elif (mesInicial = '03') or (mesFinal = '03'):
+elif mesInicial == '03' or mesFinal == '03':
 
-elif (mesInicial = '04') or (mesFinal = '04'):
+elif mesInicial == '04' or mesFinal == '04':
         
-elif (mesInicial = '05') or (mesFinal = '05'):
+elif mesInicial == '05' or mesFinal == '05':
 
-elif (mesInicial = '07') or (mesFinal = '06'):
+elif mesInicial == '07' or mesFinal == '06':
         
-elif (mesInicial = '08') or (mesFinal = '08'):
+elif mesInicial == '08' or mesFinal == '08':
 
-elif (mesInicial = '08') or (mesFinal = '08'):
+elif mesInicial == '08' or mesFinal == '08':
         
-elif (mesInicial = '09') or (mesFinal = '09'):
+elif mesInicial == '09' or mesFinal == '09':
         
-elif (mesInicial = '10') or (mesFinal = '10'):
+elif mesInicial == '10' or mesFinal == '10':
         
-elif (mesInicial = '11') or (mesFinal = '11'):
+elif mesInicial == '11' or mesFinal == '11':
 
-elif (mesInicial = '12') or (mesFinal = '12'):
+elif mesInicial == '12' or mesFinal == '12':
 
 else:
-print ('Data inválida.')
+    print ('Data inválida.'
         SystemExit
